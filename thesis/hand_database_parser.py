919f1b70767dce9848a9d58ae9a4d2525b07dcac
hellovertex <hellovertex@outlook.com>
2022-01-17 19:33:20 +0100

    added empty module that will later parse hand database files

---
+++ thesis/hand_database_parser.py
@@ -0,0 +1,4 @@
+""" This module will
+ - read .txt files inside ./data/ and
+ - parse them to create corresponding environment states.
+ - The states are encoded as in neuron_poker.gym_env.env.HoldemTable  """
