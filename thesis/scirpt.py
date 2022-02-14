b5463f1a212722e428e6388c0d39a87d89b3c918
hellovertex <hellovertex@outlook.com>
2022-02-14 16:55:11 +0100

    delted unused

--- thesis/scirpt.py
+++
@@ -1,8 +0,0 @@
-import os
-import pandas as pd
-filename = 'Atalante-1-2-USD-NoLimitHoldem-PokerStarsPA-1-16-2022.txt'
-file_dir = '../data/'  # os.path.dirname(os.path.realpath(__file__))
-file_path = os.path.join(file_dir, f"train_data/{filename}.txt")
-
-df = pd.read_csv(file_path)
-print(df.head())
