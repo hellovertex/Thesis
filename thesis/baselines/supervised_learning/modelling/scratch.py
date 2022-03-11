85c640b85e8da963f0eecac83874f535e0245e07
hellovertex <hellovertex@outlook.com>
2022-03-11 14:12:15 +0100

    ready for hotfixing data

--- thesis/baselines/supervised_learning/modelling/scratch.py
+++ thesis/baselines/supervised_learning/modelling/scratch.py
@@ -1,14 +1,45 @@
+from __future__ import annotations
+
 import argparse
-import glob
-import pandas as pd
+from functools import partial
+from os import listdir, mkdir
+from os.path import isfile, join, abspath, basename, exists
+from torch.utils.data import ConcatDataset, DataLoader
+
 import numpy as np
+import pandas as pd
 import torch
 
-from functools import partial
-from os import listdir
-from os.path import isfile, join, abspath
-
 DATA_DIR = '../../../../data/'
+
+
+class SingleTxtFileDataset(torch.utils.data.Dataset):
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+        self._data: pd.DataFrame | None = None  # todo sort
+        self._has_been_consumed = False
+
+    def load_file(self) -> pd.DataFrame:
+        # loading
+        df = pd.read_csv(self.file_path, sep=",")
+
+        # preprocesing
+        fn_to_numeric = partial(pd.to_numeric, errors="coerce")
+        self._has_been_consumed = False
+        return df.apply(fn_to_numeric).dropna().astype(dtype=np.float32)
+
+    def __getitem__(self, idx):
+        if self._data is None:
+            self._data = self.load_file()
+        y = self._data.loc[idx].pop('label')
+        return self._data.loc[idx], y
+
+    def __len__(self):
+        if self._data is None:
+            return 0
+        return len(self._data)
+
+
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(
@@ -35,32 +66,33 @@ if __name__ == "__main__":
     print(train_dir_files)
     print(train_files)
 
-    # preprocessing
-    df = pd.read_csv(train_files[0], sep=",")
-    from functools import partial
-
-    fn = partial(pd.to_numeric, errors="coerce")
-    df = df.apply(fn).dropna().astype(dtype=np.float32)
-    print(df.memory_usage(index=True, deep=True).sum())
-    print(df.head())
-
-    ys = df.pop('label')
-    print(df.head())
-
+    # list_of_datasets = [SingleTxtFileDataset(train_file) for train_file in train_files]
+    # multiple_file_dataset = ConcatDataset(list_of_datasets)
+    # loader = DataLoader(dataset=multiple_file_dataset, batch_size=1)
+    # print(len(list_of_datasets))
+    # print(len(multiple_file_dataset))
+    # for batch_ndx, sample in enumerate(loader):
+    #     print('batch_ndx:', batch_ndx, 'sample:', sample)
+    #     break
 
     # write to disk [args.preprocessed_dir]
+    preprocessed_dir = args.preprocessed_dir
+    if preprocessed_dir is None:
+        preprocessed_dir = DATA_DIR + 'train_data/0.25_0.50/preprocessed/'
+
+    def one_time_fix():
+        for file_path in train_files:
+            df = pd.read_csv(file_path, sep=",")
+            fn_to_numeric = partial(pd.to_numeric, errors="coerce")
+            df = df.apply(fn_to_numeric).dropna().astype(dtype=np.float32)
+            out_file = abspath(preprocessed_dir + basename(file_path))
+            if not exists(preprocessed_dir):
+                mkdir(preprocessed_dir)
+            df.to_csv(out_file, index_label='label', mode='w+')
+            print(f'written to {out_file}')
+    one_time_fix()
+
     # check kaggle for visualization tips
     # check machine learning from multiple text files
-    def load_file(f):
-        return None
-
-
-    class MyDataset(torch.utils.Dataset):
-        def __init__(self):
-            self.data_files = listdir('data_dir')  # todo sort
-
-        def __getindex__(self, idx):
-            return load_file(self.data_files[idx])
-
-        def __len__(self):
-            return len(self.data_files)
+    # setup mlflow locally
+    # setup mlflow in azure asai runs locally
