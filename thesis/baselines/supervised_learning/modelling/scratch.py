cebed48fa1c78ac73afc9d41eb7a775a10f24bf1
hellovertex <hellovertex@outlook.com>
2022-03-13 16:14:28 +0100

    added concatdatset from multiple txt files

--- thesis/baselines/supervised_learning/modelling/scratch.py
+++ thesis/baselines/supervised_learning/modelling/scratch.py
@@ -5,6 +5,7 @@ from functools import partial
 from os import listdir, mkdir
 from os.path import isfile, join, abspath, basename, exists
 from torch.utils.data import ConcatDataset, DataLoader
+from typing import Tuple
 
 import numpy as np
 import pandas as pd
@@ -15,30 +16,47 @@ DATA_DIR = '../../../../data/'
 
 class SingleTxtFileDataset(torch.utils.data.Dataset):
     def __init__(self, file_path: str):
+        print(f"Initializing file {file_path}")
         self.file_path = file_path
-        self._data: pd.DataFrame | None = None  # todo sort
-        self._has_been_consumed = False
+        self._data: torch.Tensor | None = None  # todo sort
+        self._labels: torch.Tensor | None = None
+        self._len = self._get_len()
 
-    def load_file(self) -> pd.DataFrame:
+    def _get_len(self):
+        """Get line count of large files cheaply"""
+        with open(self.file_path, 'rb') as f:
+            lines = 0
+            buf_size = 1024 * 1024
+            read_f = f.raw.read if hasattr(f, 'raw') and hasattr(f.raw, 'read') else f.read
+
+            buf = read_f(buf_size)
+            while buf:
+                lines += buf.count(b'\n')
+                buf = read_f(buf_size)
+
+        return lines
+
+    def load_file(self):
         # loading
         df = pd.read_csv(self.file_path, sep=",")
 
         # preprocesing
         fn_to_numeric = partial(pd.to_numeric, errors="coerce")
-        self._has_been_consumed = False
-        return df.apply(fn_to_numeric).dropna().astype(dtype=np.float32)
+        df = df.apply(fn_to_numeric).dropna().astype(dtype=np.float32)
+        labels = df.pop('label')
+        assert len(df.index) > 0
+        self._data = torch.tensor(df.values)
+        self._labels = torch.tensor(labels.values)
 
     def __getitem__(self, idx):
         if self._data is None:
-            self._data = self.load_file()
-        y = self._data.loc[idx].pop('label')
-        return self._data.loc[idx], y
+            assert self._labels is None
+            self.load_file()
+
+        return self._data[idx], self._labels[idx]
 
     def __len__(self):
-        if self._data is None:
-            return 0
-        return len(self._data)
-
+        return self._len
 
 
 if __name__ == "__main__":
@@ -54,7 +72,12 @@ if __name__ == "__main__":
     args, _ = argparser.parse_known_args()
     train_dir = args.train_dir
     if args.train_dir is None:
-        train_dir = DATA_DIR + 'train_data/0.25_0.50/'
+        train_dir = DATA_DIR + 'train_data/0.25_0.50/preprocessed'
+
+    # # write to disk [args.preprocessed_dir]
+    # preprocessed_dir = args.preprocessed_dir
+    # if preprocessed_dir is None:
+    #     preprocessed_dir = DATA_DIR + 'train_data/0.25_0.50/preprocessed/'
 
     # get list of .txt-files inside train_dir
     train_dir_files = [join(train_dir, f) for f in listdir(train_dir) if isfile(join(train_dir, f))]
@@ -66,31 +89,26 @@ if __name__ == "__main__":
     print(train_dir_files)
     print(train_files)
 
-    # list_of_datasets = [SingleTxtFileDataset(train_file) for train_file in train_files]
-    # multiple_file_dataset = ConcatDataset(list_of_datasets)
-    # loader = DataLoader(dataset=multiple_file_dataset, batch_size=1)
-    # print(len(list_of_datasets))
-    # print(len(multiple_file_dataset))
-    # for batch_ndx, sample in enumerate(loader):
-    #     print('batch_ndx:', batch_ndx, 'sample:', sample)
-    #     break
+    list_of_datasets = [SingleTxtFileDataset(train_file) for train_file in train_files]
+    multiple_file_dataset = ConcatDataset(list_of_datasets)
+    loader = DataLoader(dataset=multiple_file_dataset, batch_size=1)
+    print(len(list_of_datasets))
+    print(len(multiple_file_dataset))
+    for batch_ndx, sample in enumerate(loader):
+        print('batch_ndx:', batch_ndx, 'sample:', sample)
+        break
 
-    # write to disk [args.preprocessed_dir]
-    preprocessed_dir = args.preprocessed_dir
-    if preprocessed_dir is None:
-        preprocessed_dir = DATA_DIR + 'train_data/0.25_0.50/preprocessed/'
-
-    def one_time_fix():
-        for file_path in train_files:
-            df = pd.read_csv(file_path, sep=",")
-            fn_to_numeric = partial(pd.to_numeric, errors="coerce")
-            df = df.apply(fn_to_numeric).dropna().astype(dtype=np.float32)
-            out_file = abspath(preprocessed_dir + basename(file_path))
-            if not exists(preprocessed_dir):
-                mkdir(preprocessed_dir)
-            df.to_csv(out_file, index_label='label', mode='w+')
-            print(f'written to {out_file}')
-    one_time_fix()
+    # def one_time_fix():
+    #     for file_path in train_files:
+    #         df = pd.read_csv(file_path, sep=",")
+    #         fn_to_numeric = partial(pd.to_numeric, errors="coerce")
+    #         df = df.apply(fn_to_numeric).dropna().astype(dtype=np.float32)
+    #         out_file = abspath(preprocessed_dir + basename(file_path))
+    #         if not exists(preprocessed_dir):
+    #             mkdir(preprocessed_dir)
+    #         df.to_csv(out_file, index_label='label', mode='w+')
+    #         print(f'written to {out_file}')
+    # one_time_fix()
 
     # check kaggle for visualization tips
     # check machine learning from multiple text files
