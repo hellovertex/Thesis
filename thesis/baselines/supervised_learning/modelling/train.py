d4b483c313d3b6a97a74a4abae06aaa64a4b2a50
hellovertex <hellovertex@outlook.com>
2022-03-21 12:34:29 +0100

    updated training and provided setup script

--- thesis/baselines/supervised_learning/modelling/train.py
+++ thesis/baselines/supervised_learning/modelling/train.py
@@ -1,8 +1,11 @@
-from __future__ import annotations
+# from __future__ import annotations
 
 import argparse
+import glob
+import io
 import os
 import tempfile
+import zipfile
 
 import mlflow
 import mlflow.pytorch
@@ -13,7 +16,7 @@ from dataset import get_dataloaders
 from model import Net, train, test
 from azureml.core import Workspace
 
-DATA_DIR = '../../../../data/'
+# DATA_DIR = '../../../../data/'
 BATCH_SIZE = 64
 
 
@@ -41,6 +44,20 @@ torch.manual_seed(args.seed)
 device = torch.device("cuda" if use_cuda else "cpu")
 
 kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+
+
+def extract(filename, out_dir):
+    z = zipfile.ZipFile(filename)
+    for f in z.namelist():
+        try:
+            os.mkdir(out_dir)
+        except FileExistsError:
+            pass
+        # read inner zip file into bytes buffer
+        content = io.BytesIO(z.read(f))
+        zip_file = zipfile.ZipFile(content)
+        for i in zip_file.namelist():
+            zip_file.extract(i, out_dir)
 
 
 def run(train_loader, test_loader):
@@ -78,7 +95,41 @@ def run(train_loader, test_loader):
 
 
 if __name__ == "__main__":
-    train_dir = DATA_DIR + '03_preprocessed/0.25-0.50/preprocessed'
+    argparser = argparse.ArgumentParser(
+        description="Provide model pipeline with necessary arguments: "
+                    "- path to training data "
+                    "-whatever else comes to my mind later")
+    argparser.add_argument('-t', '--train_dir',
+                           help='abs or rel path to .txt files with raw training samples.')
+    # todo pass path to setup.py and make setup return mount context here
+    # todo mount context can depend on _Offline or Run context
+    cmd_args, _ = argparser.parse_known_args()
+    #
+    # train_dir = cmd_args.train_dir
+    # if cmd_args.train_dir is None:
+    #     # train_dir = DATA_DIR + '03_preprocessed/0.25-0.50/preprocessed'
+    #     train_dir = '03_preprocessed/0.25-0.50/preprocessed'
+
+    # # Connect to Workspace and reference Dataset
+    # ws = Workspace.from_config()
+    # dataset = ws.datasets["preprocessed"]
+    #
+    # print(dataset.name)
+    #
+    # # Create mountcontext and mount the dataset
+    # mount_ctx = dataset.mount()
+    # mount_ctx.start()
+    #
+    # # Get the mount point
+    # train_dir = mount_ctx.mount_point
+    # train_dir = "Users/sascha.lange/github.com/hellovertex/Thesis/data/03_preprocessed/0.25-0.50"
+    train_dir = "Users/sascha.lange/github.com/hellovertex/Thesis/data/03_preprocessed/0.25-0.50/preprocessed"
+    print(f"Train_DIR: {train_dir}")
+    print(f'FILES INSIDE: = {os.listdir(train_dir)}')
+    # train_dir = DATA_DIR + '03_preprocessed/0.25-0.50/preprocessed'
+    # zipfiles = glob.glob(train_dir.__str__() + '/*.zip', recursive=False)
+    # print(f"zipfiles = {zipfiles}")
+    # [extract(zipfile, out_dir=train_dir) for zipfile in zipfiles]
 
     dataloaders = get_dataloaders(train_dir)
     train_loader = dataloaders['train']
---
+++ thesis/setup.sh
@@ -0,0 +1,10 @@
+echo "Running setup script"
+pip install requests
+cd PokerEnv
+pip install PokerRL
+pip install torch
+pip install gdown
+pip install click
+pip install mlflow
+pip install tensorboardX
+echo "Finished setup script"
