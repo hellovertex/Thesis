a43e6e9ad927739a795b8c0c4c6fce9fd28bf9ff
Sascha <hellovertex@outlook.com>
2022-04-27 23:26:56 +0200

    fixed request model

--- thesis/api/src/calls/environment/configure.py
+++ thesis/api/src/calls/environment/configure.py
@@ -1,21 +1,18 @@
 from fastapi import APIRouter
-from src.model.environment_config import EnvironmentConfig
+from src.model.environment_config import EnvironmentConfig, EnvironmentConfigRequestBody
 from starlette.requests import Request
-
-router = APIRouter()
-
 
 @router.post("/environment/configure",
              response_model=EnvironmentConfig,
              operation_id="configure_environment")
-async def configure_environment(request: Request,
-                                n_players: int,
-                                starting_stack_size: int):
+async def configure_environment(request: Request, config: EnvironmentConfigRequestBody):
     """Creates an environment in the backend and returns its unique ID.
     Use this ID with /reset and /step endpoints to play the game.
 
     Internal: Calls backend.Backend.make_environment(...),
     returns its id and config wrapped in EnvironmentConfig Model class"""
+    n_players = config.n_players
+    starting_stack_size = config.starting_stack_size
     assert 2 <= n_players <= 6
     # make args for env
     print(n_players)
@@ -26,3 +23,6 @@ async def configure_environment(request: Request,
     return EnvironmentConfig(env_id=request.app.backend.make_environment(config),
                              num_players=n_players,
                              starting_stack_size=starting_stack_size)
+
+
+router = APIRouter()
--- thesis/api/src/model/environment_config.py
+++ thesis/api/src/model/environment_config.py
@@ -1,13 +1,28 @@
+from typing import Optional
+
 from pydantic import BaseModel, Field
 
 
+class EnvironmentConfigRequestBody(BaseModel):
+    num_players: int = Field(
+        ...,
+        example=6,
+        description="The environments number of starting players. 2 <= num_players <= 6"
+    )
+    starting_stack_size: int = Field(
+        ...,
+        example=20000,
+        description="The number of chips each player will get on resetting the environment."
+    )
+
+
 class EnvironmentConfig(BaseModel):
-    # env_id: int = Field(
-    #     ...,
-    #     example=1,
-    #     description="The environment unique id "
-    #                 "used for requesting this specific environment."
-    # )
+    env_id: Optional[int] = Field(
+        ...,
+        example=1,
+        description="The environment unique id "
+                    "used for requesting this specific environment."
+    )
     num_players: int = Field(
         ...,
         example=6,
